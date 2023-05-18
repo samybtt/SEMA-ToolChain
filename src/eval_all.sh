ee4aca5f00578aa0f45dd818c3656d79be43feb4
samybtt <samy.bettaieb@student.uclouvain.be>
2023-05-18 14:45:35 +0000

    save vm state

--- src/eval_all.sh
+++ src/eval_all.sh
@@ -3,15 +3,16 @@ search_dir="/root/final/"
 # RandMethodArray=("CSTOCH CSTOCH2 CSTOCHset CSTOCHset2 WSELECT WSELECT2 WSELECTset WSELECTset2 STOCH")
 RandMethodArray=("CSTOCH CSTOCH2 CSTOCHSET2 STOCH")
 MethodArray=("WSELECT WSELECT2 WSELECTSET2 CDFS CBFS")
-FamArray=("bancteian ircbot sillyp2p sytro simbot FeakerStealer sfone lamer RedLineStealer RemcosRAT Sodinokibi delf nitol gandcrab wabot")
+# FamArray=("bancteian ircbot sillyp2p sytro simbot FeakerStealer sfone lamer RedLineStealer RemcosRAT Sodinokibi delf nitol gandcrab wabot")
 # FamArray=("lamer RedLineStealer Sodinokibi sillyp2p")
+FamArray=("RedLineStealer FeakerStealer sfone lamer delf RemcosRAT Sodinokibi wabot gandcrab simbot sytro sillyp2p ircbot nitol bancteian")
 
 j="0"
 echo $j
 for family in ${FamArray[*]}
 do
     echo family
-    for file in $(ls $search_dir/$family | head -n 42 | tail -n 10)
+    for file in $(ls $search_dir/$family | tail -n 30 | head -n 10)
     do
         echo $file
         for method in ${RandMethodArray[*]}
